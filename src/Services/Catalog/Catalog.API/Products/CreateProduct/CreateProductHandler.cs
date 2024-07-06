63437812a57b740e26ad41e8ac6c80cd1a6e85c9
Purple The Cat <saihunfhone@gmail.com>
2024-07-07 01:26:56 +0700

    Add 3 endpoint

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,34 @@
+﻿namespace Catalog.API.Products.CreateProduct;
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+        : ICommand<CreateProductResult>;
+public record CreateProductResult(Guid Id);
+
+public class GetProductHandler(IDocumentSession session)
+    : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        /* Steps to do 
+         * ------------------
+         * 1. Create product entity from command object
+         * 2. Save to database
+         * 3. Return CreateProductResult
+        */
+
+        //  1. Create product entity from command object
+        var product = new Product
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+
+        // 2. Save to Database
+        session.Store(product);
+        await session.SaveChangesAsync();
+
+        return new CreateProductResult(product.Id);
+    }
+}
